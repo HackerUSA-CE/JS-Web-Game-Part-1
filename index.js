6e305bc8c7dee4a0e397db33ad89a361dbdb7a75
Joshua Miles <joshua.m@hackerusa.com>
2021-02-09 13:13:48 -0600

    added starter code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)
