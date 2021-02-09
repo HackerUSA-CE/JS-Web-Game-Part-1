4affd880ac584a44e85f52d5d41a943c8b5d1070
Joshua Miles <joshua.m@hackerusa.com>
2021-02-09 13:10:37 -0600

    finished with bonus

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            makeObject(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 function makeObject(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -16,6 +24,14 @@ function makeItem(url, left, bottom){
     })
 }
 
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+
 makeObject('assets/green-character.gif', 100, 100)
 makeObject('assets/tree.png', 200, 300)
 makeObject('assets/pillar.png', 350, 100)
@@ -24,5 +40,5 @@ makeObject('assets/crate.png', 150, 200)
 
 makeItem('assets/shield.png', 165, 185)
 makeItem('assets/staff.png', 600, 100)
-makeItem('assets/well.png', 500, 400)
+makeItem('assets/well.png', 500, 425)
 makeItem('assets/sword.png', 500, 405)
