f92c0df6cf32649af074352a15f4708729139d61
Joshua Miles <joshua.m@hackerusa.com>
2021-02-09 12:00:31 -0600

    solution

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function makeObject(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
 
+function makeItem(url, left, bottom){
+    let object = makeObject(url, left, bottom)
+
+    object.addEventListener('click', () => {
+        object.remove()
+    })
+}
+
+makeObject('assets/green-character.gif', 100, 100)
+makeObject('assets/tree.png', 200, 300)
+makeObject('assets/pillar.png', 350, 100)
+makeObject('assets/pine-tree.png', 450, 200)
+makeObject('assets/crate.png', 150, 200)
+
+makeItem('assets/shield.png', 165, 185)
+makeItem('assets/staff.png', 600, 100)
+makeItem('assets/well.png', 500, 400)
+makeItem('assets/sword.png', 500, 405)
