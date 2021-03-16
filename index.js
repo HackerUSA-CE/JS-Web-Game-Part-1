4fa170c5709fb7a9068c6c8afc080c867da2ba73
Joshua Miles <joshua.m@hackerusa.com>
2021-03-16 09:58:39 -0500

    -

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function makeObject(url, left, bottom){
+function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
@@ -8,21 +8,21 @@ function makeObject(url, left, bottom){
     return object
 }
 
-function makeItem(url, left, bottom){
-    let object = makeObject(url, left, bottom)
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
     object.addEventListener('dblclick', () => {
         object.remove()
     })
 }
 
-makeObject('assets/green-character.gif', 100, 100)
-makeObject('assets/tree.png', 200, 300)
-makeObject('assets/pillar.png', 350, 100)
-makeObject('assets/pine-tree.png', 450, 200)
-makeObject('assets/crate.png', 150, 200)
-makeObject('assets/well.png', 500, 425)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
 
-makeItem('assets/sword.png', 500, 405)
-makeItem('assets/shield.png', 165, 185)
-makeItem('assets/staff.png', 600, 100)
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
