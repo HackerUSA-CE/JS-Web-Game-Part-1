6470ce3687af9c79c0df71d3c0555d8ef0374c80
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-08-16 20:23:04 -0500

    backtracking to debug code

--- index.js
+++ index.js
@@ -70,28 +70,3 @@ newItem('assets/sword.png',500,405)
 newItem('assets/shield.png',165,185)
 newItem('assets/staff.png',600,100)
 
-/*let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
-
-sword.addEventListener('click', function(){
-    sword.remove()
-})*/
-
-/*let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)*/
-
-/*let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)*/
-
